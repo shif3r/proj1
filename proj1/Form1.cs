178e58451c873e1a21e0a5d1a498ca7ebe85b67f
Rodionov Viktor <denomit07@gmail.com>
2018-07-02 12:55:28 +0600

    Update #1
    Creating visual studio C# project and setting up first version of interface with primitive functional.

---
+++ proj1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proj1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox3.Text);
+            double summ = num1 + num2;
+            textBox2.Text = summ.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox3.Text);
+            double summ = num1 - num2;
+            textBox2.Text = summ.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox3.Text);
+            double summ = num1 * num2;
+            textBox2.Text = summ.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox3.Text);
+            double summ = num1 / num2;
+            textBox2.Text = summ.ToString();
+        }
+    }
+}

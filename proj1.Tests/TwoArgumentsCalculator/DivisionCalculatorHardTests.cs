1aaf71256fd4e22bd9c501a38f12586184f7e6e9
Rodionov Viktor <denomit07@gmail.com>
2018-07-04 11:47:42 +0600

    Update #15
    Adding 3 advanced tests for simple math functions

---
+++ proj1.Tests/TwoArgumentsCalculator/DivisionCalculatorHardTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+namespace proj1.Tests.TwoArgumentsCalculator
+{
+    [TestFixture]
+    public class DivisionCalculatorHardTests
+    {
+        [TestCase(2, 2, 1)]
+        [TestCase(8, 4, 2)]
+        [TestCase(-16, -4, 4)]
+        public void CalculateTest(double firstValue, double secondValue, double expected)
+        {
+            ITwoArgumentsCalculator calculator = TwoArgumentsFactory.CreateCalculator("Divide");
+            var actualResult = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
---
+++ proj1.Tests/TwoArgumentsCalculator/MaxCalculatorHardTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+namespace proj1.Tests.TwoArgumentsCalculator
+{
+    [TestFixture]
+    public class MaxCalculatorHardTests
+    {
+        [TestCase(2, 4, 4)]
+        [TestCase(8, 4, 8)]
+        [TestCase(-16, -4, -4)]
+        public void CalculateTest(double firstValue, double secondValue, double expected)
+        {
+            ITwoArgumentsCalculator calculator = TwoArgumentsFactory.CreateCalculator("Maximum");
+            var actualResult = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}

e2632b12f6d993240c239161befdd55f87f057c8
Rodionov Viktor <denomit07@gmail.com>
2018-07-04 10:39:59 +0600

    Update #12
    Refactoring

--- proj1/TwoArgumentsCalculators/RemainderCalculator.cs
+++ proj1/TwoArgumentsCalculators/RemainderCalculator.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace proj1.TwoArgumentsCalculators
+﻿namespace proj1.TwoArgumentsCalculators
 {
     public class RemainderCalculator : ITwoArgumentsCalculator
     {
         double ITwoArgumentsCalculator.Calculate(double firstNumber, double secondNumber)
         {
-            return firstNumber%secondNumber;
+            return firstNumber % secondNumber;
         }
     }
 }

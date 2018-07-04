e09571229e2a981ba3830f21f1768c5c1df1d4b1
Pokemon-Catcher <varuvarukillme@gmail.com>
2018-07-04 12:01:19 +0600

    Update #16
    Tested factories

---
+++ proj1.Tests/SingleArgumentCalculator/SingleArgumentCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using proj1.SingleArgumentCalculators;
+
+namespace proj1.Tests.SingleArgumentCalculator
+{
+    [TestFixture]
+    public class SingleArgumentCalculatorTests
+    {
+        [TestCase("Cosinus", typeof(CosinusCalculator))]
+        [TestCase("Sinus", typeof(SinusCalculator))]
+        [TestCase("Loge", typeof(LogECalculator))]
+        [TestCase("Pow2", typeof(Pow2Calculator))]
+        [TestCase("Logarifm10", typeof(Logarifm10Calculator))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = SingleArgumentFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
---
+++ proj1.Tests/TwoArgumentsCalculator/TwoArgumentsCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using proj1.TwoArgumentsCalculators;
+using System;
+
+namespace proj1.Tests.TwoArgumentsCalculator
+{
+    [TestFixture]
+    public class TwoArgumentsCalculatorTests
+    {
+        [TestCase("Plus", typeof(AddictionCalculator))]
+        [TestCase("Minus", typeof(SubstractionCalculator))]
+        [TestCase("Divide", typeof(DivisionCalculator))]
+        [TestCase("Multiply", typeof(MultiplicationCalculator))]
+        [TestCase("Maximum", typeof(MaxCalculator))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoArgumentsFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
